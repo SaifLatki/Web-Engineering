efe14aa1119bbf6613ec13ad81234e40ae097b98
Saif <saifullahlatki338@gmail.com>
2024-11-27 23:31:33 +0500

    use of forOf

---
+++ JavaScript/EcmaScript 06/forOf.js
@@ -0,0 +1,18 @@
+// for of loop is used to iterate over the elements of an array or string. 
+//It is a new way to loop over the elements of an array. It is introduced in ECMAScript 6.
+
+//using for of loop to iterate over the elements of an array
+const cars = ['BMW', 'Audi', 'Mercedes', 'Toyota', 'Ford'];
+
+for (const car of cars) {
+    console.log(car);
+}
+
+//using for of loop to iterate over the characters of a string
+const str = 'hello';
+
+for (const char of str) {
+    console.log(char);
+}
+
+
