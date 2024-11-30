2aede0e0db90b5b96df81e0f9335c50c573b5e9d
Saif <saifullahlatki338@gmail.com>
2024-11-30 23:13:04 +0500

    class js done

---
+++ JavaScript/EcmaScript 06/class.js
@@ -0,0 +1,13 @@
+class student{
+    constructor(name,id,age){
+        this.name=name;
+        this.id=id;
+        this.age=age;
+    }
+}
+
+const s1=new student("ali",2340,24);
+const s2=new student("fiza",3456,20);
+
+console.log("Student 1:",s1);
+console.log("Student 2:",s2);
