780c8f86e96dc0cb59e043aa1aec1f341a8dda78
Saif <saifullahlatki338@gmail.com>
2024-12-04 22:49:16 +0500

    random quote task done

--- Promises Lab/RandomQuotes/script.js
+++ Promises Lab/RandomQuotes/script.js
@@ -1,26 +1,25 @@
 const button = document.getElementById('btn');
-        const text = document.getElementById('showQuotes');
+const text = document.getElementById('showQuotes');
+let qt = document.getElementById('quote');
+let at = document.getElementById('author');
 
-        function getQuote() {
-            const url = 'https://api.api-ninjas.com/v1/quotes?category=happiness';
-            fetch(url, {
-                method: 'GET',
-                headers: { 'X-Api-Key': 'YOUR_API_KEY' }
-            })
-            .then(response => {
-                console.log('Response:', response);
-                if (!response.ok) {
-                    throw new Error(`HTTP error! Status: ${response.status}`);
-                }
-                return response.json();
-            })
-            .then(data => {
-                const [quoteData] = data;
-                const { quote, author } = quoteData;
-                console.log(`Quote: ${quote}, Author: ${author}`);
-            })
-            .catch(error => console.error(`Error fetching quote data: ${error.message}`));
+function getQuote() {
+    const url = 'https://quotes-api-self.vercel.app/quote';
+    fetch(url )
+    .then(response => {
+        // console.log('Response:', response);
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
         }
-        
-
-        button.addEventListener('click', getQuote);
+        return response.json();
+    })
+    .then(data => {
+        const { quote, author } = data;
+        console.log(`Quote: ${quote}, Author: ${author} `);
+        qt.innerHTML = quote ;
+        at.innerHTML = author;
+       
+    })
+    .catch(error => console.error(`Error fetching quote data: ${error.message}`));
+}
+button.addEventListener('click', getQuote);
